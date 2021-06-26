da38d3875f66cdc1dfb0d10dd9bd45998b2464d9
ericlndbrg <ericrlindberg@gmail.com>
2021-06-26 12:55:31 -0700

    added a deck class, require it in blackjack.rb

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 
 require_relative 'classes/card'
+require_relative 'classes/deck'
---
+++ classes/deck.rb
@@ -0,0 +1,19 @@
+class Deck
+  # represents a deck of cards
+  attr_accessor :cards
+
+  def initialize
+    cards = []
+    suits = ['s', 'h', 'c', 'd']
+    numbers = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+
+    suits.each do |suit|
+      numbers.each do |number|
+        card = Card.new(number, suit)
+        cards.push(card)
+      end
+    end
+    self.cards = cards
+  end
+
+end
